815b90fbc64194e7430d0ade5f336cff31ca8647
noni <ojc123004@gmail.com>
2022-04-23 17:07:01 +0900

    hello

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,40 @@
 package hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.*;
 
-@RestController
-public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+@Controller
+public class HelloController{
+
+    @RequestMapping(value = "/")
+    public String index() 
+    {
+        return "home";
     }
+    @RequestMapping("hello")
+        public String hellostring(@RequestParam("name") String name) 
+        {
+            return "hello" + name;
+        }
+    @RequestMapping(value = "/myview")
+    public String contentview(Model model)
+    {
+        model.addAttribute("value","hi");
+        return "myview";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+
+        return "hello-template";
+    }
+
 }
 
---
+++ src/main/java/hello/mvcConfig.java
@@ -0,0 +1,18 @@
+// package hello;
+
+// import org.springframework.beans.factory.annotation.Configurable;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.context.WebApplicationContext;
+// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// class WebMvcConfiguration implements WebMvcConfigurer
+// {
+//     @Override
+//     public void addResourceHandlers(final ResourceHandlerRegistry registry)
+//     {
+//         registry.addResourceHandler("/")
+//             .addResourceLocations("/static/");
+//     }
+// }
